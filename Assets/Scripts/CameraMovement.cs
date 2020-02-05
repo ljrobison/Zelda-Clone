ae9e0839f8a10f52264dba2d280727e18cf71628
Lee Robison <ljrobison@live.com>
2020-02-04 20:47:10 -0500

    Added Unity project files.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPostion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+    }
+
+    //LateUpdate is called after all Update functions have been called.
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPostion = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPostion.x = Mathf.Clamp(targetPostion.x, minPostion.x, maxPosition.x);
+            targetPostion.y = Mathf.Clamp(targetPostion.y, minPostion.y, maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position, targetPostion, smoothing);
+        }
+    }
+}
