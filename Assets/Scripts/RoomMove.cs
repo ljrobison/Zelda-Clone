12603883bd90a067d67d5a3e763522f91bdc0d59
Lee Robison <ljrobison@live.com>
2020-02-04 23:25:57 -0500

    Added room transition script.

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class RoomMove : MonoBehaviour
 {
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main.GetComponent<CameraMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minPostion += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange;
+        }        
     }
 
     //TODO: Get the camera to snap to the new boundaries when a player transitions rooms.
