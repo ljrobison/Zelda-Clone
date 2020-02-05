55f7eb81a124dcac03f6aa21ac37b4c345ca5630
Lee Robison <ljrobison@live.com>
2020-02-05 01:39:16 -0500

    Work on Place Text for entering new area.

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
